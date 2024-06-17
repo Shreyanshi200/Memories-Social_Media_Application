fc3051d62b9972149f6841023ff5d57c7bf55a0c
Shreyanshi200 <120807907+Shreyanshi200@users.noreply.github.com>
2024-06-17 15:04:00 +0530

    adding vercel file

--- server/index.js
+++ server/index.js
@@ -14,21 +14,23 @@ const app = Express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    // origin:["https://"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
 
-// app.use(Express.static(path.join(__dirname, './client/build')))
-// app.get("*",function(req,res){
-//   res.sendFile(path.join(__dirname, "./client/build/index.js"))
-// });
-
 
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
+  res.json("hello");
   res.redirect("/posts");
 });
 
